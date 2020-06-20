95aa66d656dd097018d2626eef023fe412e26103
rupabrio <rupabr.io@gmail.com>
2020-06-19 23:47:31 -0400

    avance

--- engine/instabot.py
+++ engine/instabot.py
@@ -5,10 +5,16 @@ from ultra_secreto import clave
 
 
 
-class Bot():
+class Bot:
 	def __init__(self,usuario,clave):
+		self.user = usuario
+		self.pw = clave
 		self.driver = webdriver.Firefox()
+
+	def navegar(self):
 		self.driver.get('https://www.instagram.com')
+
+	def loggeo(self):
 		sleep(5)
 		self.driver.find_element_by_name('username')\
 			.send_keys(usuario)
@@ -16,5 +22,22 @@ class Bot():
 			.send_keys(clave)
 		self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div')\
 			.click()
-		
-my_confidence = Bot(usuario,clave)
+
+	def skip(self):
+		self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')\
+			.click()
+		self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')\
+			.click()
+
+
+	def entrar(self):
+		self.navegar()
+		sleep(5)
+		self.loggeo()
+		sleep(5)
+		self.skip()
+
+
+test = Bot(usuario,clave)
+test.entrar()
+
--- engine/ultra_secreto.py
+++ engine/ultra_secreto.py
@@ -1,2 +1,2 @@
-usuario = ''
-clave = ''
+usuario = 'kaimankilla'
+clave = 'XxX#123'
