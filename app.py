d95fb544ffddc79290d2899c8a8a1b41d95b6299
rupabrio <rupabr.io@gmail.com>
2020-06-20 00:13:45 -0400

    Implementado menu linea de comando

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from sys import argv
 
 from utils.menu import *
 
-from engine import instablot
+from engine import instabot
+
+mi_objetivo_de_clase = Bot()
 
 if __name__ == '__main__':
     # Menu de entrada
