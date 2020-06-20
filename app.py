638d2d3d0801d76bb0d7c1de7f630bcae5266b37
avgbndt <avgbndt@gmail.com>
2020-06-20 00:06:02 -0500

    Menu linea de comando implementado

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+#
 from sys import argv
-
+import getpass
+#
 from utils.menu import *
-
 from engine import bots
 
 if __name__ == '__main__':
+
     # Menu de entrada
     menu = """1. Log in | 2. Go to user | 3. Exit"""
     arguments = argv[1:]
     show_banner()
     print("Press Enter to Start Bot, Press CTR C to cancel")
     input("GO?")
+
+    #Bot instance
     uname = input("username=> ")
-    pw = input("passwd=> ")
+    pw = getpass.getpass("passwd=> ")
     my_bot = bots.Bot(uname, pw)
+
+    #Menu loop
     while True:
         print(menu)
         option = input("=> ")
